0cc8b02895c28e7940609c908ca08b49aaabd940
Fred Emmott <mail@fredemmott.co.uk>
2012-01-20 20:08:09 +0000

    add tests for HtmlSelfClosingElement and HtmlSingletonElement

---
+++ spec/html_self_closing_element_spec.rb
@@ -0,0 +1,62 @@
+require File.dirname(__FILE__) + '/_helper.rb'
+require 'rxhp/html_self_closing_element'
+
+describe Rxhp::HtmlSelfClosingElement do
+  describe '#render' do
+    before :each do
+      @it = Rxhp::HtmlSelfClosingElement.new
+      def @it.tag_name; 'foo'; end
+    end
+
+    context 'in HTML' do
+      context 'with no children' do
+        before :each do
+          @out = @it.render(:format => Rxhp::HTML_FORMAT)
+        end
+
+        it 'should create an opening tag' do
+          @out.should include '<foo>'
+        end
+
+        it 'should create a closing tag' do
+          @out.should include '</foo>'
+        end
+      end
+    end
+      
+    context 'in tiny HTML' do
+      context 'with no children' do
+        before :each do
+          @out = @it.render(:format => Rxhp::TINY_HTML_FORMAT)
+        end
+
+        it 'should create an opening tag' do
+          @out.should include '<foo>'
+        end
+
+        it 'should not create a closing tag' do
+          @out.should_not include '</foo>'
+        end
+      end
+
+      context 'with children' do
+        before :each do
+          @it.children.push 'bar'
+          @out = @it.render(:format => Rxhp::TINY_HTML_FORMAT)
+        end
+
+        it 'should create an opening tag' do
+          @out.should include '<foo>'
+        end
+
+        it 'should not create a closing tag' do
+          @out.should_not include '</foo>'
+        end
+
+        it 'should incldue the children' do
+          @out.should include 'bar'
+        end
+      end
+    end
+  end
+end
---
+++ spec/html_singleton_element_spec.rb
@@ -0,0 +1,50 @@
+require File.dirname(__FILE__) + '/_helper.rb'
+require 'rxhp/html_singleton_element.rb'
+
+describe Rxhp::HtmlSingletonElement do
+  describe '#render' do
+    before :each do
+      @it = Rxhp::HtmlSingletonElement.new
+      def @it.tag_name; 'foo'; end
+    end
+
+    it 'raises a ScriptError if there are children' do
+      @it.children.push 'bar'
+      lambda { @it.render }.should raise_error(Rxhp::ScriptError)
+    end
+
+    context 'in HTML mode' do
+      it 'should create an opening tag' do
+        @it.render(
+          :format => Rxhp::HTML_FORMAT
+        ).should include '<foo>'
+      end
+
+      it 'should not include a closing tag' do
+        @it.render(
+          :format => Rxhp::HTML_FORMAT
+        ).should_not include '</foo>'
+      end
+    end
+
+    context 'in XHTML mode' do
+      it 'should create a self-closing tag' do
+        @it.render(
+          :format => Rxhp::XHTML_FORMAT
+        ).should include '<foo />'
+      end
+
+      it 'should not create an opening tag' do
+        @it.render(
+          :format => Rxhp::XHTML_FORMAT
+        ).should_not include '<foo>'
+      end
+
+      it 'should not include a closing tag' do
+        @it.render(
+          :format => Rxhp::XHTML_FORMAT
+        ).should_not include '</foo>'
+      end
+    end
+  end
+end
