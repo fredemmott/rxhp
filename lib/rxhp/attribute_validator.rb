d355fc493782ecd39137b974daa70ebadc9cdc1d
Fred Emmott <mail@fredemmott.co.uk>
2012-01-25 22:40:30 +0000

    look ma, docs

--- lib/rxhp/attribute_validator.rb
+++ lib/rxhp/attribute_validator.rb
@@ -1,7 +1,25 @@
 require 'rxhp/error'
 
 module Rxhp
+  # Provide support for limiting or requiring attributes.
+  #
+  # Attributes can be:
+  # * whitelisted (see {ClassMethods#accept_attributes})
+  # * required (see {ClassMethods#require_attributes})
+  #
+  # Requiring an attribute automaticaly whitelists it.
+  #
+  # To use:
+  # * +include+ this module
+  # * whitelist/require attributes as appropriate
+  # * call {#validate_attributes!} from wherever seems appropriate - such
+  #   {Rxhp::Element#validate!}
+  #
+  # Subclasses will automatically have validated attributes, and will
+  # copy the rules from their parent class. Additional attributes can be
+  # accepted or required in subclasses without affecting the parent class.
   module AttributeValidator
+    # Indicates that a provided attribute was not whitelisted.
     class UnacceptableAttributeError < ValidationError
       attr_reader :element, :attribute, :value
       def initialize element, attribute, value
@@ -10,6 +28,7 @@ module Rxhp
       end
     end
 
+    # Indicates that a required attribute was not provided.
     class MissingRequiredAttributeError < ValidationError
       attr_reader :element, :attribute
       def initialize element, attribute
@@ -18,6 +37,13 @@ module Rxhp
       end
     end
 
+    # Whether or not the attributes are acceptable.
+    #
+    # If you need more details than just "it's invalid", call
+    # {#validate_attributes!} instead and examine the exceptions it raises.
+    #
+    # @return whether or not the attributes are all whitelisted, and all
+    #   required attributes are provided.
     def valid_attributes?
       begin
         self.validate_attributes!
@@ -27,6 +53,14 @@ module Rxhp
       end
     end
 
+    # Check if attributes are valid, and raise an exception if they're not.
+    #
+    # @raise {MissingRequiredAttributeError} if an attribute that is
+    #   required was not provided.
+    # @raise {UnacceptableAttributeError} if a non-whitelisted attribute
+    #   was provided.
+    # @return [true] if the attribute are all valid, and all required
+    #   attributes are provided.
     def validate_attributes!
       # Check for required attributes
       self.class.required_attributes.each do |matcher|
@@ -72,6 +106,56 @@ module Rxhp
       end
     end
 
+    module ClassMethods
+      attr_accessor :acceptable_attributes, :required_attributes
+      # Accept all attributes matching the specified pattern.
+      #
+      # @param pattern can be a:
+      #   +Symbol+:: must equal the attribute name after converting to a
+      #              +String+, and replacing underscores with hyphens.
+      #   +Object+:: must match the attribute name with +===+.
+      #   +Array+:: the members must be a any of the above that matches the
+      #             attribute name.
+      #   +Hash+:: the key can be any of the above that matches the
+      #            attribute name; the value is matched against the
+      #            attribute value in the same way.
+      # @example
+      #   accept_attribute 'id'
+      #   accept_attribute /^data-/
+      #   accept_attributes ['href', 'src']
+      #   accept_attributes ({ 'type' => ['checkbox', 'text', 'submit'] })
+      def accept_attributes pattern
+        acceptable_attributes.push pattern
+      end
+      alias :accept_attribute :accept_attributes
+
+      # Require an attribute matching the specified pattern.
+      #
+      # @param pattern accepts the same values as {#accept_attributes}
+      def require_attributes pattern
+        accept_attributes pattern
+        required_attributes.push pattern
+      end
+      alias :require_attribute :require_attributes
+
+      # Accept any attributes whatsoever.
+      def accept_all_attributes
+        accept_attributes Object
+      end
+
+      private
+
+      def inherited(subklass) # @Private @api
+        Rxhp::AttributeValidator.inherited(subklass)
+      end
+    end
+
+    private
+
+    def self.name_from_symbol symbol
+      symbol.to_s.gsub('_', '-')
+    end
+
     def self.match_value? matcher, value
       if matcher == String
         return (value.is_a? String) || (value.is_a? Symbol)
@@ -91,47 +175,24 @@ module Rxhp
       end
     end
 
-    def self.name_from_symbol symbol
-      symbol.to_s.gsub('_', '-')
-    end
-
+    # Include class methods, and initialize class variables.
     def self.included(klass)
       klass.extend(ClassMethods)
-      class << klass
-        attr_accessor :acceptable_attributes, :required_attributes
-        def accept_attributes matcher
-          acceptable_attributes.push matcher
-        end
-        alias :accept_attribute :accept_attributes
-
-        def require_attributes matcher
-          accept_attributes matcher
-          required_attributes.push matcher
-        end
-        alias :require_attribute :require_attributes
-
-        def accept_all_attributes
-          accept_attributes Object
-        end
-      end
 
       klass.acceptable_attributes = []
       klass.required_attributes = []
     end
 
+    # Make subclasses validated too.
+    #
+    # Their accept/require lists will be copied from their superclass, but
+    # can be modified separately.
     def self.inherited(subklass)
       subklass.class_eval do
         include Rxhp::AttributeValidator
       end
       subklass.acceptable_attributes = subklass.superclass.acceptable_attributes.dup
       subklass.required_attributes = subklass.superclass.required_attributes.dup
-    end
-
-    module ClassMethods
-      def inherited(subklass)
-        Rxhp::AttributeValidator.inherited(subklass)
-      end
-
     end
   end
 end
